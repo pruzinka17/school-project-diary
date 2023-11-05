254cc1dcc01f2b83380e5ec2549843af59c78d06
Miroslav Bořek <boremiro@seznam.cz>
2023-11-05 20:00:33 +0100

    Initial Commit

---
+++ DenikProjekt/DenikProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  DenikProjektApp.swift
+//  DenikProjekt
+//
+//  Created by Miroslav Bořek on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct DenikProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
